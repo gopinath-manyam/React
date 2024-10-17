a2032244fe901283ab2015f55f9dd04e04578fa1
gopinath-manyam <gopinath.iiitn@gmail.com>
2024-10-17 11:28:56 +0530

    Project structure, Package manager & Parcel bundler

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
-const heading = React.createElement("h1",{id:"h1"},"React");
+import React from "react";
+import ReactDOM from "react-dom";
+
+const heading = React.createElement("h1", { id: "h1" }, "React");
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 root.render(heading);
